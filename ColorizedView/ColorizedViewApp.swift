e21575ad4fd4c146dff1d67e7d61461e8b0d30be
AnastasyiaMaximova <rsk.maximova@bk.ru>
2025-01-22 20:19:47 +0300

    Initial Commit

---
+++ ColorizedView/ColorizedViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedViewApp.swift
+//  ColorizedView
+//
+//  Created by Anastasya Maximova on 22.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
