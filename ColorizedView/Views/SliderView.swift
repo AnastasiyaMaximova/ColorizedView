e2cd4ef94c9283a0579b0afe5121c2b416320540
AnastasyiaMaximova <rsk.maximova@bk.ru>
2025-01-23 22:36:59 +0300

    WTF

---
+++ ColorizedView/Views/SliderView.swift
@@ -0,0 +1,21 @@
+//
+//  SliderView.swift
+//  ColorizedView
+//
+//  Created by Anastasya Maximova on 22.01.2025.
+//
+
+import SwiftUI
+
+//struct SliderView: View {
+//    
+//    @State private var value: Double
+//    
+//    var body: some View {
+//        Slider(value: $value, in: 0...250, step: 1)
+//    }
+//}
+
+//#Preview {
+//    SliderView(value: 1.0)
+//}
