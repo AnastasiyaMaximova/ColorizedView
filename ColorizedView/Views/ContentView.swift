ebc6f8838f9e7e8ae0cb575122e4ea613ff56aa2
AnastasyiaMaximova <rsk.maximova@bk.ru>
2025-01-25 15:52:35 +0300

    work with logic

--- ColorizedView/Views/ContentView.swift
+++ ColorizedView/Views/ContentView.swift
@@ -7,131 +7,91 @@
 
 import SwiftUI
 
-
 struct ContentView: View {
+   
     @State private var redSliderValue = Double.random(in: 0...255)
     @State private var greenSliderValue = Double.random(in: 0...255)
     @State private var blueSliderValue = Double.random(in: 0...255)
-    @State private var redText = "0,5"
-    @State private var greenText = "0,5"
-    @State private var blueText = "0,5"
+    @State private var redText = ""
+    @State private var greenText = ""
+    @State private var blueText = ""
     @State private var isPresented = false
-    @State private var validateText = ""
-   
-  
+    @State private var doneButtonPressed = false
+    @State private var redValidateText = ""
+    @State private var greenValidateText = ""
+    @State private var blueValidateText = ""
+
     
     var body: some View {
-        VStack {
-            Color(
-                red: redSliderValue,
-                green: greenSliderValue,
-                blue: blueSliderValue
+            VStack {
+                Color(setColor())
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20, style: .continuous)
+                            .stroke(Color.white, lineWidth: 6)
+                    )
+                    .frame(height: 200)
+                    .padding(.bottom, 20)
+                
+                SliderView(
+                    sliderColor: .red,
+                    sliderValue: $redSliderValue,
+                    text: $redText,
+                    isPresented: $isPresented,
+                    validatedText: $redValidateText,
+                    doneButtonPressed: $doneButtonPressed
                 )
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20, style: .continuous)
-                        .stroke(Color.white, lineWidth: 6)
+                SliderView(
+                    sliderColor: .green,
+                    sliderValue: $greenSliderValue,
+                    text: $greenText,
+                    isPresented: $isPresented,
+                    validatedText: $greenValidateText,
+                    doneButtonPressed: $doneButtonPressed
                 )
-                .frame(height: 200)
-                .padding(.bottom, 20)
-            
-            SliderView(
-                sliderValue: $redSliderValue,
-                sliderColor: SliderColor.red.color,
-                text: $redText,
-                isPresented: $isPresented,
-                validatedText: $validateText
-            )
-            SliderView(
-                sliderValue: $greenSliderValue,
-                sliderColor: SliderColor.green.color,
-                text: $greenText,
-                isPresented: $isPresented,
-                validatedText: $validateText
-            )
-            SliderView(
-                sliderValue: $blueSliderValue,
-                sliderColor: SliderColor.blue.color,
-                text: $blueText,
-                isPresented: $isPresented,
-                validatedText: $validateText
-            )
-            Spacer()
-        }
-        .padding()
-        .containerRelativeFrame([.horizontal, .vertical])
-        .background(.cyan)
-        
-        .toolbar{
-            ToolbarItemGroup(placement: .keyboard) {
-                Button("Done"){
-                }
-                .background(
-                    RoundedRectangle(cornerRadius: 6, style: .continuous)
-                        .fill(.white)
+                SliderView(
+                    sliderColor: .blue,
+                    sliderValue: $blueSliderValue,
+                    text: $blueText,
+                    isPresented: $isPresented,
+                    validatedText: $blueValidateText,
+                    doneButtonPressed: $doneButtonPressed
                 )
-                .foregroundStyle(.black)
-                .font(.headline)
-                .shadow(radius: 3)
-                .alert("Wrong format!", isPresented: $isPresented){
-                    Button("OK"){}
-                } message: {
-                    Text("Please, enter correct value.")
-                }
+                Spacer()
             }
-        }
-    }
-    
-    private enum SliderColor {
-        case red
-        case green
-        case blue
-        var color: Color {
-            switch self{
-            case .red: return .red
-            case .green: return .green
-            case .blue: return .blue
+            .padding()
+            .containerRelativeFrame([.horizontal, .vertical])
+            .background(.cyan)
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done"){
+                        doneButtonPressed = true
+                    }
+                    .background(
+                        RoundedRectangle(cornerRadius: 5, style: .continuous)
+                            .fill(.white)
+                    )
+                    .foregroundStyle(.black)
+                    .font(.subheadline)
+                    .shadow(radius: 3)
+                    .alert("Wrong format!", isPresented: $isPresented){
+                        Button("OK"){}
+                    } message: {
+                        Text("Please, enter correct value.")
+                    }
+                }
+                
             }
-        }
     }
     
-    private func validText(text: String?){
-        guard let validText = text else {return}
-        if validText.isEmpty || !(0...255).contains(Double(validText) ?? 0){
-            isPresented.toggle()
-            return
-        }
-        switch validText {
-        case redText:
-            validateText = redText
-            redSliderValue = Double(validText) ?? 0
-        case greenText:
-            validateText = greenText
-            greenSliderValue = Double(validText) ?? 0
-        default:
-            validateText = blueText
-            blueSliderValue = Double(validText) ?? 0
-        }
+    private func setColor() -> Color {
+        return  Color(
+            red: redSliderValue/255,
+            green: greenSliderValue/255,
+            blue: blueSliderValue/255
+        )
     }
-//    private func setColor() -> Color {
-//        return Color(
-//                red: redSliderValue,
-//                green: greenSliderValue,
-//                blue: blueSliderValue
-//                )
-//    }
-
-//    private func setColor() {
-//        colorView.backgroundColor = UIColor(
-//            red: CGFloat(redSlider.value),
-//            green: CGFloat(greenSlider.value),
-//            blue: CGFloat(blueSlider.value),
-//            alpha: 1
-//        )
-//    }
-    
-    
-    
 }
 
 #Preview {
@@ -139,16 +99,17 @@ struct ContentView: View {
 }
 
 struct SliderView: View {
-    
-    @Binding var sliderValue: Double
     var sliderColor: Color
+
+    @Binding var sliderValue: Double
     @Binding var text: String
     @Binding var isPresented: Bool
     @Binding var validatedText: String
+    @Binding var doneButtonPressed: Bool
     
     var body: some View {
         HStack{
-            Text(sliderValue.formatted())
+            Text(string(value: sliderValue))
                 .font(.title3)
                 .bold()
                 .foregroundStyle(.white)
@@ -160,6 +121,13 @@ struct SliderView: View {
             )
             .tint(sliderColor)
             .padding(.horizontal, 5)
+            .onAppear {
+                text = string(value: sliderValue)
+            }
+            .onChange(of: sliderValue) {
+                    text = string(value: sliderValue)
+
+            }
             
             TextField("", text: $text)
                 .font(.title3)
@@ -168,38 +136,40 @@ struct SliderView: View {
                 .frame(width: 60, height: 40, alignment: .center)
                 .textFieldStyle(.roundedBorder)
                 .keyboardType(.decimalPad)
-//                .toolbar{
-//                    ToolbarItemGroup(placement: .keyboard) {
-//                        Button("Done"){
-//                           validText()
-//                            sliderValue = Double(validatedText) ?? 0
-//                        }
-//                        .background(
-//                            RoundedRectangle(cornerRadius: 6, style: .continuous)
-//                                .fill(.white)
-//                        )
-//                        .foregroundStyle(.black)
-//                        .font(.headline)
-//                        .shadow(radius: 3)
-//                        .alert("Wrong format!", isPresented: $isPresented){
-//                            Button("OK"){
-//                                text = ""
-//                            }
-//                        } message: {
-//                            Text("Please, enter correct value.")                            }
-//                    }
-//                }
+                .onChange(of: text){
+                    if doneButtonPressed{
+                        if !isPresented{
+                            validText()
+                            setSlider()
+                            doneButtonPressed.toggle()
+                        }
+                    }
+                }
         }
                 
         }
     
     private func validText(){
-        if text.isEmpty || !(0...255).contains(Double(text)!){
+        guard let validText = Double(text) else {
             isPresented.toggle()
             return
         }
-        validatedText = text
+        if !(0...255).contains(validText){
+            isPresented.toggle()
+            return
+        }
+        validatedText = string(value: validText)
     
     }
     
+    private func setSlider() {
+        if let value = Double(validatedText) {
+            sliderValue = value
+        }
+    }
+
+}
+
+private func string(value:Double) -> String {
+    String(format: "%.0f", value)
 }
